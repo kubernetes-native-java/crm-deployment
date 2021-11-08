026894244ff15aeaffe0e0d4b9184dc14dbce44a
Josh Long <josh@joshlong.com>
2021-11-08 01:36:54 -0800

    deploy

---
+++ .github/workflows/deploy.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+set -e
+
+ROOT=${GITHUB_WORKSPACE:-$(cd $(dirname $0)/../.. && pwd)}
+
+function image_name() {
+  echo "gcr.io/${GCLOUD_PROJECT}/$1"
+}
+
+function app_push() {
+  NAME=$1
+  IMAGE_NAME=$(image_name "$NAME")
+  OUTPUT_FN=$ROOT/tmp/${NAME}-generated.yaml
+  mkdir -p "$(dirname $OUTPUT_FN)" || echo "could not create ${OUTPUT_FN}."
+  cat $ROOT/.github/workflows/k8s.yaml.template |
+    sed -e 's,<NS>,'${K8S_NS}',g' |
+    sed -e 's,<APP>,'${NAME}',g' |
+    sed -e 's,<GCR_PROJECT>,'${GCLOUD_PROJECT}',g' >$OUTPUT_FN
+  cat "$OUTPUT_FN"
+  kubectl delete ns/$K8S_NS || echo "couldn't delete the $K8S_NS namespace. Maybe it doesn't exist?"
+  kubectl apply -f "$OUTPUT_FN" -n "$K8S_NS" || echo "could not deploy."
+}
+
+echo "starting in $ROOT. "
+cd $ROOT/../..
+ls -la
+kubectl delete ns/${K8S_NS}
+app_push customers
+app_push orders
